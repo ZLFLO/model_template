263c39ee1e0d673bd882461aeef91a87bc17edd0
martinvonk <vonk.mart@gmail.com>
2025-08-12 15:25:40 +0200

    ruff

--- src/settings.py
+++ src/settings.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-model_name = "MODEL_NAME" # change model name
-extent = [0.0, 1.0, 0.0, 1.0] # change extent to model extent
+model_name = "MODEL_NAME"  # change model name
+extent = [0.0, 1.0, 0.0, 1.0]  # change extent to model extent
 
 paths = {
-    "data" : Path(__file__).parent / "data", # set path to data folder
-    "models" : Path(__file__).parent / "models", # change path to models folder
+    "data": Path(__file__).parent / "data",  # set path to data folder
+    "models": Path(__file__).parent / "models",  # change path to models folder
 }
