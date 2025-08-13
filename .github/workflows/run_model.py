eae381f10d85e4dbc7275cc2d332db38261c1abb
martinvonk <vonk.mart@gmail.com>
2025-08-13 15:59:45 +0200

    update run_model workflow

--- .github/workflows/run_model.py
+++ .github/workflows/run_model.py
@@ -2,31 +2,22 @@ import subprocess
 import sys
 from pathlib import Path
 
-SRC_DIR = Path(__file__).parent.parent.parent / "src"
-SRC_DIR = SRC_DIR.resolve()
+src_dir = (Path(__file__).parent.parent.parent / "src").resolve()
 
-
-def find_python_files(directory):
-    return directory.rglob("*.py")
+py_files = [
+    src_dir / "settings.py",
+    # add relevent pyfiles in order
+]
 
 
 def main():
-    errors = []
-    for py_file in find_python_files(SRC_DIR):
-        print(f"Running: {py_file}")
+    for py_file in py_files:
         result = subprocess.run([sys.executable, str(py_file)], capture_output=True)
         if result.returncode != 0:
-            errors.append((py_file, result.stderr.decode()))
             print(f"Error in {py_file}:\n{result.stderr.decode()}")
+            sys.exit(1)
         else:
             print(f"{py_file} ran successfully.")
-    if errors:
-        print("\nSummary of errors:")
-        for file, err in errors:
-            print(f"{file}:\n{err}")
-        sys.exit(1)
-    else:
-        print("\nAll files ran successfully.")
 
 
 if __name__ == "__main__":
