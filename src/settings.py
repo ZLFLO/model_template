81ca859f3b6dc1164bb058e60623879349085798
OnnoEbbens <onnoebbens@gmail.com>
2025-08-13 14:47:33 +0200

    add data directory structure

--- src/settings.py
+++ src/settings.py
@@ -4,7 +4,7 @@ model_name = "MODEL_NAME"  # change model name
 extent = [0.0, 1.0, 0.0, 1.0]  # change extent to model extent
 
 paths = {
-    "models": Path(__file__).parent / "models",  # change path to models folder
-    "download": Path(__file__).parent.parent
-    / "download",  # set path to download folder
+    "models": Path(__file__).parent.parent / "modeldata/models",  # change path to models folder
+    "downloads": Path(__file__).parent.parent
+    / "modeldata/downloads",  # set path to download folder
 }
